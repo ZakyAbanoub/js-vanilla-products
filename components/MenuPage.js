12719009c79849408720bb015d0aab94113dc4c5
Abanoub Zaky <abanoub.zaky@caffeina.com>
2024-09-20 00:04:57 +0300

    add menu list, add items to cart and details page

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -20,6 +20,35 @@ export class MenuPage extends HTMLElement {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+    this.render();
+  }
+
+  render() {
+    const menuEl = this.root.querySelector("#menu");
+    if (app.store.menu) {
+      // Clear the content
+      menuEl.innerHTML = "";
+      for (let category of app.store.menu) {
+        const liCategory = document.createElement("li");
+        liCategory.innerHTML = `
+            <h3>${category.name}</h3>
+            <ul class="category"></ul>
+            `;
+        menuEl.appendChild(liCategory);
+
+        category.products.forEach((product) => {
+          const item = document.createElement("product-item");
+          item.dataset.product = JSON.stringify(product);
+          liCategory.querySelector("ul").appendChild(item);
+        });
+      }
+    } else {
+      menuEl.innerHTML = "Loading...";
+    }
   }
 }
 
